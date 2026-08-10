ce05caf516e6aac6244003bf80c6ca0f4b3c37c7
Bilal Dar <daraftab32@gmail.com>
2026-08-10 23:03:47 +0530

    Fix Gmail SMTP IPv4 connection

--- server/server.js
+++ server/server.js
@@ -55,17 +55,18 @@ app.post("/api/contact", async (req, res) => {
     // ===============================
     // GMAIL SMTP
     // ===============================
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
 
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+  family: 4,
 
-      auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_APP_PASSWORD,
-      },
-    });
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_APP_PASSWORD,
+  },
+});
 
 
     // ===============================
